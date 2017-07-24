a6eddb07d7582727398be6e2c5a35e49cd29147f
mark.lees <mark.lees@petards.com>
2017-07-24 10:33:16 +0100

    Added latest puzzle

--- do.sh
+++ do.sh
@@ -1,2 +1,2 @@
-./build/puzzle 8 8 oihoyedhmsotttkodepspctarsogicoracpnrrspoblraeoapbloatinculcihct cupboard comb rocket panties pigsty carrot tooth card chain lollipop shoes -v | tee p.out
+./build/puzzle 8 8 glalslsmeoselurgrsrrbpeauaioetlutftmorflceseyoshihcblrotppnodlib 7 4 4 5 7 6 5 5 8 8 5 
 
