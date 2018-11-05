53691fb3a9a78b140eccd91493d606754dd13445
Mark <mark@localhost.localdomain>
2018-11-05 13:05:40 +0000

    Added a load of solutions

--- bld.sh
+++ bld.sh
@@ -2,7 +2,7 @@
 
 [[ ! -d build ]] && mkdir -p build
 
-cd build && cmake .. && make
+cd build && cmake .. && make -j 10 
 
 # g++ -Wall -Wpedantic -O6 -fno-exceptions  *.cpp -o puzzle -lpthread -ltbb
 # g++ -DINTERNAL_WORD -Wall -Wpedantic -O6 -fno-exceptions  *.cpp -o npuzzle -lpthread -ltbb
